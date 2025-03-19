5501b1022c8cc77389c5d80579c8de92d5680dd1
bernabarros <bernardobarros.work@gmail.com>
2025-03-19 18:03:30 +0000

    Add method doc

--- LevelOne/MainIsHere.cs
+++ LevelOne/MainIsHere.cs
@@ -4,6 +4,10 @@ namespace LevelOne
 {
     public class Program
     {
+        /// <summary>
+        /// This is the Main() method
+        /// </summary>
+        /// <param name="args">Console arguments go here</param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
